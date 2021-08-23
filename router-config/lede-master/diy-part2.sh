aad86c02b7935c1e719fbaa7acb3e57e3af261f9
slcun <slcun@live.com>
2021-08-23 09:49:30 +0800

    Update diy-part2.sh

--- router-config/lede-master/diy-part2.sh
+++ router-config/lede-master/diy-part2.sh
@@ -50,8 +50,6 @@ svn co https://github.com/hubutui/p7zip-lede/trunk package/p7zip
 
 git clone https://github.com/jerrykuku/luci-app-ttnode.git package/lean/luci-app-ttnode
 
-svn co https://github.com/ophub/luci-app-amlogic/trunk/luci-app-amlogic package/luci-app-amlogic
-
 
 # Apply patch
 # git apply ../router-config/patches/{0001*,0002*}.patch --directory=feeds/luci
